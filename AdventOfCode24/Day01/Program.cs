009114301ea5d07039b259e4c74728522c77a102
Daniel Sklenitzka <daniel.sklenitzka@softaware.at>
2024-12-05 09:42:06 +0100

    Day 01

---
+++ AdventOfCode24/Day01/Program.cs
@@ -0,0 +1,20 @@
+﻿var pairs = File.ReadAllLines("input.txt")
+                .Select(ParseLine);
+
+var firstList = GetList(pairs, 0);
+var secondList = GetList(pairs, 1);
+
+var difference = firstList.Zip(secondList, (f, s) => Math.Abs(f - s)).Sum();
+
+var similarityScore = firstList.Select(f => f * secondList.Count(s => s == f)).Sum();
+
+Console.WriteLine(difference);
+Console.WriteLine(similarityScore);
+
+static int[] ParseLine(string line) =>
+    line.Split(' ', StringSplitOptions.RemoveEmptyEntries)
+        .Select(int.Parse)
+        .ToArray();
+
+static int[] GetList(IEnumerable<int[]> pairs, int index) =>
+    pairs.Select(p => p[index]).Order().ToArray();
