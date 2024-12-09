c84cb1993d36905595a1cd59ffe001c130153532
Daniel Sklenitzka <daniel.sklenitzka@softaware.at>
2024-12-09 21:47:47 +0100

    Day 03

---
+++ AdventOfCode24/Day03/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Text.RegularExpressions;
+
+static partial class Program
+{
+    [GeneratedRegex(@"mul\((\d{1,3}),(\d{1,3})\)")]
+    private static partial Regex MulRegex();
+
+    [GeneratedRegex(@"do\(\)|don't\(\)|mul\((\d{1,3}),(\d{1,3})\)")]
+    private static partial Regex DoMulRegex();
+
+    public static void Main()
+    {
+        var input = File.ReadAllText("input.txt");
+
+        var sum = MulRegex()
+            .Matches(input)
+            .Sum(Mul);
+
+        Console.WriteLine(sum);
+
+        var enable = true;
+        var enabledSum = 0;
+        foreach (Match match in DoMulRegex().Matches(input))
+        {
+            switch (match.Value)
+            {
+                case "do()":
+                    enable = true;
+                    break;
+
+                case "don't()":
+                    enable = false;
+                    break;
+
+                default:
+                    if (enable)
+                    {
+                        enabledSum += Mul(match);
+                    }
+                    break;
+            }
+        }
+
+        Console.WriteLine(enabledSum);
+    }
+
+    private static int Mul(Match match) =>
+        int.Parse(match.Groups[1].Value) *
+        int.Parse(match.Groups[2].Value);
+}
